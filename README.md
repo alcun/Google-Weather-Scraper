d41b9f846ae862e9587e3f3c641c3742a9d586df
alcun <95560342+alcun@users.noreply.github.com>
2021-12-11 04:34:34 +0000

    Added README and TO DO Added comments to illustrate roles of commands

---
+++ README.md
@@ -0,0 +1,13 @@
+Using python to scrape and display weather data for queried location from google weather api.
+
+Change the 'query value' for different results.
+
+CURRENT VERSION:
+Works as intended, displaying queried location, temp, unit and desc
+
+TO DO:
+I could add:
+time
+weather forecasts for a few days ahead
+a way to export result as .csv file
+a ui where the user is prompted for query and parameters like time
--- main.py
+++ main.py
@@ -6,11 +6,11 @@ s= HTMLSession()
 '''f string lets us put what ever is in query into variable'''
 
 
-query = 'chicago'
+query = 'Chicago'
 url = f'https://www.google.com/search?q=weather+{query}'
 
 
-
+''' User agent helps us bypass captcha type things'''
 
 r = s.get(url, headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36'})
 
@@ -26,6 +26,7 @@ unit = (r.html.find('div.vk_bk.wob-unit span.wob_t', first=True).text)
 
 desc = (r.html.find('div.VQF4g', first=True).find('span#wob_dc', first=True).text)
 
+print(query, temp, unit, desc)
 
 
 '''element names'''
