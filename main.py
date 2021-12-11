e1f640c5e9cc636c7a28c309984f87e9cf7ef9a0
alcun <95560342+alcun@users.noreply.github.com>
2021-12-11 03:47:39 +0000

    .text gives us title of page i.e Weather Liverpool

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ url = f'https://www.google.com/search?q=weather+liverpool{query}'
 r = s.get(url, headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36'})
 
 
-print (r.html.find('title', first=True))
+print (r.html.find('title', first=True.text))
 
 
 
